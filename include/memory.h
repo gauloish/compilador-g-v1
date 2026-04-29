c0167b56f44b348f4aee1fad4f8b8067eacd17a3
gauloish <125079293+gauloish@users.noreply.github.com>
2026-04-29 15:27:33 -0300

    refactor: remove unecessary code

--- include/memory.h
+++ include/memory.h
@@ -4,10 +4,7 @@
 #ifndef __MEMORY_H__
 #define __MEMORY_H__
 
-void begin_memory(void);
-void end_memory(void);
-
 void* allocate_memory(const size_t);
-void free_memory(const void*);
+void free_memory(void*);
 
 #endif /* __MEMORY_H__ */
--- source/memory.c
+++ source/memory.c
@@ -6,19 +6,6 @@
 extern void yyerror(char*);
 extern bool analysis_error;
 
-typedef struct _MemoryNode MemoryNode;
-
-/**
- * @brief Node list for allocated memory
- * 
- */
-struct _MemoryNode {
-    void* memory;
-    MemoryNode* next;
-};
-
-MemoryNode* node = NULL;
-
 /**
  * @brief End the program due to insufficient memory
  * 
@@ -29,65 +16,18 @@ void out_of_memory(void) {
 }
 
 /**
- * @brief Initialize the global memory list
- * 
- */
-void begin_memory(void) {
-    node = (MemoryNode*) malloc(sizeof(MemoryNode));
-
-    if (node == NULL) {
-        out_of_memory();
-    }
-
-    *node = (MemoryNode){
-        .memory = NULL,
-        .next = NULL,
-    };
-}
-
-/**
- * @brief End the global memory list and free all allocated memory
- * 
- */
-void end_memory(void) {
-    while (node != NULL) {
-        MemoryNode* next = node->next;
-
-        free(node->memory);
-        free(node);
-
-        node = next;
-    }
-
-    node = NULL;
-}
-
-/**
- * @brief Allocate memory for the program and store it in the init of global memory list
+ * @brief Allocate memory for the program
  * 
  * @param size Size of allocated memory
  * @return void* Pointer for allocated memory
  */
 void* allocate_memory(const size_t size) {
-    MemoryNode* next = (MemoryNode*) malloc(sizeof(MemoryNode));
-
-    if (next == NULL) {
-        out_of_memory();
-    }
-
     void* memory = malloc(size);
 
     if (memory == NULL) {
-        free(next);
         out_of_memory();
     }
 
-    *next = (MemoryNode){
-        .memory = memory,
-        .next = node,
-    };
-
-    node = next;
     return memory;
 }
 
@@ -96,26 +36,10 @@ void* allocate_memory(const size_t size) {
  * 
  * @param pointer Pointer for the memory to be deallocated
  */
-void free_memory(const void* pointer) {
+void free_memory(void* pointer) {
     if (pointer == NULL) {
         return;
     }
 
-    MemoryNode* current = node;
-
-    while (current != NULL) {
-        if (current->memory == pointer) {
-            MemoryNode* next = current->next;
-
-            free(current->memory);
-
-            current->memory = next->memory;
-            current->next = next->next;
-
-            free(next);
-            break;
-        }
-
-        current = current->next;
-    }
+    free(pointer);
 }
